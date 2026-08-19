b59d481e7c618a7ab8823fc874fb6511f73d5c11
leeeeeseojin <kkyuuu00@gmail.com>
2026-08-20 05:09:51 +0900

    feat: RecipeReviewPage 라우트 등록 (#46)

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,21 @@
-import {
-  BrowserRouter,
-  Routes,
-  Route,
-} from "react-router-dom";
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
-import RecipeLinkPage from "./pages/recipe-input-page/entry/RecipeLinkPage";
-import RecipeInputPage from "./pages/recipe-input-page/entry/RecipeInputPage";
-import ProfilePage from './pages/profile-page/entry/ProfilePage';
+import RecipeLinkPage from './pages/recipe-input-page/entry/RecipeLinkPage'
+import RecipeInputPage from './pages/recipe-input-page/entry/RecipeInputPage'
+import ProfilePage from './pages/profile-page/entry/ProfilePage'
+import RecipeReviewPage from './pages/recipe-review-page/entry/RecipeReviewPage'
 
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<ProfilePage />} />
-        <Route path="/recipe/input" element={<RecipeInputPage />}/>
-        <Route path="/recipe/link" element={<RecipeLinkPage />}/>
+        <Route path='/' element={<ProfilePage />} />
+        <Route path='/recipe/input' element={<RecipeInputPage />} />
+        <Route path='/recipe/link' element={<RecipeLinkPage />} />
+        <Route path='/recipe/review' element={<RecipeReviewPage />} />
       </Routes>
     </BrowserRouter>
-  );
-};
+  )
+}
 
-export default App;
+export default App
