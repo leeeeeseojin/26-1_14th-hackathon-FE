10c918a90088fdfc27d66b263103d5669c44b51c
leeeeeseojin <kkyuuu00@gmail.com>
2026-08-20 02:52:49 +0900

    feat: Button 컴포넌트 제작 (#14)

--- src/components/common-button/CommonButton.jsx
+++ src/components/common-button/CommonButton.jsx
@@ -1,21 +1,22 @@
-import './CommonButton.css';
+import './CommonButton.css'
 
 const CommonButton = ({
   children,
   type = 'button',
+  weight = 'bold',
   onClick,
   disabled = false,
 }) => {
   return (
     <button
       type={type}
-      className="common-button"
+      className={`common-button common-button--${weight}`}
       onClick={onClick}
       disabled={disabled}
     >
       {children}
     </button>
-  );
-};
+  )
+}
 
-export default CommonButton;
+export default CommonButton
